2735a729cd8e10bd5f15e397aa51a235b5d5502a
Diego Zarur <desenvolvimento@grupolhasa.com.br>
2018-02-05 13:37:36 -0200

    first commit

--- bin/chat-server.php
+++ bin/chat-server.php
@@ -1,18 +1,30 @@
 <?php
+
+require dirname(__DIR__) . '/vendor/autoload.php';
+
 use Ratchet\Server\IoServer;
 use Ratchet\Http\HttpServer;
 use Ratchet\WebSocket\WsServer;
+use Ratchet\Http\Router;
+use Ratchet\MessageComponentInterface;  
+use Ratchet\ConnectionInterface;
 use MyApp\Chat;
 
-    require dirname(__DIR__) . '/vendor/autoload.php';
 
-    $server = IoServer::factory(
-        new HttpServer(
-            new WsServer(
-                new Chat()
-            )
-        ),
-        8080
-    );
 
-    $server->run();
+
+//$server = IoServer::factory(
+//    new HttpServer(
+//        new WsServer(
+//            new Chat()
+//        )
+//    ),
+//    8080
+//);
+
+
+// Run the server application through the WebSocket protocol on port 8080
+$app = new Ratchet\App("localhost", 8080, '0.0.0.0');
+$app->route('/chat', new Chat, array('*'));
+
+$app->run();
--- src/Chat.php
+++ src/Chat.php
@@ -1,46 +1,65 @@
-<?php
+<?php  
 namespace MyApp;
-use Ratchet\MessageComponentInterface;
-use Ratchet\ConnectionInterface;
 
-class Chat implements MessageComponentInterface {
-    protected $clients;
+use Ratchet\MessageComponentInterface;  
+use Ratchet\ConnectionInterface;
+use Guzzle\Http\QueryStringObject;
+
+
+
+class Chat implements MessageComponentInterface {  
+    public $clients;
+    private $logs;
+    private $connectedUsers;
+    private $connectedUsersNames;
 
     public function __construct() {
         $this->clients = new \SplObjectStorage;
+        $this->logs = [];
+        $this->connectedUsers = [];
+        $this->connectedUsersNames = [];
     }
 
     public function onOpen(ConnectionInterface $conn) {
-        // Store the new connection to send messages to later
+        $querystring = $conn->WebSocket->request->getQuery();
         $this->clients->attach($conn);
-        #print_r($conn);
-        #$conn->WebSocket->request->getQuery()
-        echo "New connection! ({$conn->Chat})\n";
+        //$querystring = $this->clients->WebSocket->request->getQuery();
+        print_r($querystring);
+        //echo "New connection! ({$conn->resourceId})\n";
+        //$conn->send(json_encode($this->logs));
+        //$this->connectedUsers [$conn->resourceId] = $conn;
     }
 
     public function onMessage(ConnectionInterface $from, $msg) {
-        $numRecv = count($this->clients) - 1;
-        echo sprintf('Connection %d sending message "%s" to %d other connection%s' . "\n"
-            , $from->resourceId, $msg, $numRecv, $numRecv == 1 ? '' : 's');
-
-        foreach ($this->clients as $client) {
-            if ($from !== $client) {
-                // The sender is not the receiver, send to each client connected
-                $client->send($msg);
-            }
+        // Do we have a username for this user yet?
+        if (isset($this->connectedUsersNames[$from->resourceId])) {
+            // If we do, append to the chat logs their message
+            $this->logs[] = array(
+                "user" => $this->connectedUsersNames[$from->resourceId],
+                "msg" => $msg,
+                "timestamp" => time()
+            );
+            $this->sendMessage(end($this->logs));
+        } else {
+            // If we don't this message will be their username
+            $this->connectedUsersNames[$from->resourceId] = $msg;
         }
     }
 
     public function onClose(ConnectionInterface $conn) {
-        // The connection is closed, remove it, as we can no longer send it messages
+        // Detatch everything from everywhere
         $this->clients->detach($conn);
-
-        echo "Connection {$conn->resourceId} has disconnected\n";
+        unset($this->connectedUsersNames[$conn->resourceId]);
+        unset($this->connectedUsers[$conn->resourceId]);
     }
 
     public function onError(ConnectionInterface $conn, \Exception $e) {
-        echo "An error has occurred: {$e->getMessage()}\n";
-
         $conn->close();
+    }
+
+    private function sendMessage($message) {
+        foreach ($this->connectedUsers as $user) {
+            $user->send(json_encode($message));
+        }
     }
 }
